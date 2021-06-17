eff9a84b6775750185c19fdf77a64ad115b61188
Patryk Jasik <zielonyfantom@interia.pl>
2021-06-17 22:01:47 +0200

    Added simple version of play command

---
+++ cogs/music.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+import discord, youtube_dl
+
+class Music(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def play(self, context, url : str, voice_channel='Music'):
+        channel = discord.utils.get(context.guild.voice_channels, name=voice_channel)
+        voice = discord.utils.get(self.client.voice_clients, guild=context.guild)
+        if voice == None:
+            await channel.connect()
+        else:
+            await context.send('Bot is connectedd to another channel.', delete_after=10)
+
+def setup(client):
+    client.add_cog(Music(client))
