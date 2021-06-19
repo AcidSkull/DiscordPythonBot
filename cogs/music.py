425894bcdfc6326ee692825afa73733a816df4a8
yourmoma <sds@fdd.com>
2021-06-19 12:32:32 +0200

    Upgrade the play command to play music

--- cogs/music.py
+++ cogs/music.py
@@ -1,5 +1,6 @@
+from discord.errors import ClientException
 from discord.ext import commands
-import discord, youtube_dl
+import discord, youtube_dl, os
 
 class Music(commands.Cog):
     def __init__(self, client):
@@ -7,20 +8,46 @@ class Music(commands.Cog):
 
     @commands.command()
     async def play(self, context, url : str, voice_channel='Music'):
+        music = os.path.isfile('music.mp3')
+        try:
+            if music:
+                os.remove('music.mp3')
+        except PermissionError:
+            await context.send('Wait for the current playing music to end.')
+            return
+        
         channel = discord.utils.get(context.guild.voice_channels, name=voice_channel)
-        voice = discord.utils.get(self.client.voice_clients, guild=context.guild)
-        if voice == None:
+        try:
             await channel.connect()
-        else:
-            await context.send('Bot is connectedd to another channel.', delete_after=10)
+        except ClientException:
+            pass
+        voice_client = discord.utils.get(self.client.voice_clients, guild=context.guild)
+        
+        ydl_opts = {
+            'format' : 'bestaudio/best',
+            'postprocessors' : [{
+                'key' : 'FFmpegExtractAudio',
+                'preferredcodec' : 'mp3',
+                'preferredquality' : '192',
+            }],
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        for file in os.listdir('./'):
+            if file[-4:] == '.mp3':
+                os.rename(file, 'music.mp3')
+        voice_client.play(discord.FFmpegPCMAudio('music.mp3'))
 
     @commands.command()
     async def leave(self, context):
-        voice = discord.utils.get(self.client.voice_clients, guild=context.guild)
-        if voice == None:
-            await context.send('Bot is not connected to any channel so it can\'t leave.', delete_after=10)
+        voice_client = discord.utils.get(self.client.voice_clients, guild=context.guild)
+        if voice_client == None:
+            await context.send('Bot is not connected to any channel so it can\'t leave.')
         else:
-            await voice.disconnect()
+            await voice_client.disconnect()
+
+    
 
 def setup(client):
     client.add_cog(Music(client))
